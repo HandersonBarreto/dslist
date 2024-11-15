cc682d30fd9d2551992d7482fda4ab67c4781143
Handerson Barreto <handersonaires@gmail.com>
2024-11-15 08:53:05 -0300

    seed game

--- src/main/java/com/devsuperior/DSList/entities/Game.java
+++ src/main/java/com/devsuperior/DSList/entities/Game.java
@@ -18,27 +18,32 @@ public class Game {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String title;
+
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String plalforms;
+	private String platforms;
 	private Double score;
 	private String imgUrl;
+
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 
 	public Game() {
 
 	}
 
-	public Game(long id, String title, Integer year, String genre, String plalforms, Double score, String imgUrl,
+	public Game(long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.plalforms = plalforms;
+		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
@@ -78,11 +83,11 @@ public class Game {
 	}
 
 	public String getPlalforms() {
-		return plalforms;
+		return platforms;
 	}
 
 	public void setPlalforms(String plalforms) {
-		this.plalforms = plalforms;
+		this.platforms = plalforms;
 	}
 
 	public Double getScore() {
