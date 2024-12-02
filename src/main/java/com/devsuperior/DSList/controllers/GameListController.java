e8d1e228dded7d0c98003b516ef2e1b0cb70f4c1
Handerson Barreto <handersonaires@gmail.com>
2024-12-02 11:17:16 -0300

    Pronto para implantar

--- src/main/java/com/devsuperior/DSList/controllers/GameListController.java
+++ src/main/java/com/devsuperior/DSList/controllers/GameListController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.DSList.DTO.GameListDTO;
+import com.devsuperior.DSList.DTO.GameMinDTO;
 import com.devsuperior.DSList.services.GameListService;
+import com.devsuperior.DSList.services.GameService;
 
 @RestController
 @RequestMapping(value = "/lists")
@@ -16,6 +19,9 @@ public class GameListController {
 
 	@Autowired
 	private GameListService gameListService;
+	
+	@Autowired
+	private GameService gameService;
 
 	@GetMapping
 	public List<GameListDTO> findAll() {
@@ -23,4 +29,9 @@ public class GameListController {
 		return result;
 	}
 
+	@GetMapping (value = "/{listId}/games")
+	public List<GameMinDTO> findbyList(@PathVariable Long listId) {
+		List<GameMinDTO> result = gameService.findByList(listId);
+		return result;
+	}
 }
